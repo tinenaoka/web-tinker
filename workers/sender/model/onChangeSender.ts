b49ccddb13dc831c3b3ef4054da10e0e60746988
tinenaoka <vaseevnik12@mail.ru>
2024-11-03 01:44:58 +0300

    Bug fix end scripting event and change logic run script to await for start first element

--- workers/sender/model/onChangeSender.ts
+++ workers/sender/model/onChangeSender.ts
@@ -1,18 +1,13 @@
-import {sendMessageFromExtension} from '../../../browser/runtime/model/sendMessageFromExtension';
 import {Script} from '../../../entities';
 import {useFeatureRunScript} from '../../../src/features/run-script';
+import {sendMessageFromBrowser} from '../../../browser/runtime/model/sendMessageFromBrowser';
 
-const CONFIG_CHANGE_DOM = {childList: true, subtree: true};
+const CONFIG_CHANGE_DOM = {childList: true, subtree: true, attributes: true};
 
 const runScript = useFeatureRunScript;
 
-const clickToElementFromSelector = (selector: string): boolean => {
-    let node = <HTMLElement>document.querySelector(selector);
-    if (!node) {
-        return false
-    }
+const clickToElement = (node: HTMLElement): void => {
     node.click()
-    return true;
 }
 
 const saveCurrentScriptRunning = async (): Promise<void> => {
@@ -31,29 +26,22 @@ const whitForAnimation = async (timeAnimation: number): Promise<void> => {
     return await new Promise((resolve) => setTimeout(resolve, timeAnimation));
 }
 
-const getNextElementNode = (runningScript: Array<Script> | [], index = 0): HTMLElement | null => {
-    if (runningScript.length === 0) {
-        return null
-    }
-    return document.querySelector(runningScript[index]?.selector)
+const getCurrentElementNode = (runningScript: Array<Script> | [], index = 0): HTMLElement | null => {
+    return document.querySelector(runningScript[index].selector)
 }
 
-const whitForNextElement = async (): Promise<HTMLElement | boolean> => {
+const waitForCurrentElement = async (): Promise<HTMLElement> => {
     return new Promise(async (resolve): Promise<boolean> => {
         let runningScript = await getRunningScript();
-        if (runningScript.length === 0) {
-            resolve(false);
-            return false;
-        }
-
-        let nextElement = getNextElementNode(runningScript);
+        let nextElement = getCurrentElementNode(runningScript);
         if (nextElement !== null) {
             resolve(nextElement);
             return true;
         }
 
         const observerNextElement = new MutationObserver(async (): Promise<boolean> => {
-            nextElement = getNextElementNode(runningScript);
+            console.log('observerNextElement', getCurrentElementNode(runningScript), runningScript[0])
+            nextElement = getCurrentElementNode(runningScript);
             if (nextElement !== null) {
                 resolve(nextElement);
                 observerNextElement.disconnect()
@@ -104,21 +92,21 @@ export const onChangeSender = (actionMame: string, timeAnimation = 500): void =>
             return
         }
         isCanNextScriptAction = false;
-        for (let script of await getRunningScript()) {
-            if (clickToElementFromSelector(script.selector)) {
-                console.log('-------CLICK--------')
-                await saveCurrentScriptRunning();
-                await whitForAnimation(timeAnimation);
-                let nextElement = await whitForNextElement();
-                if (typeof nextElement !== 'boolean') {
-                    await whitForElementIsShowingOnPage(nextElement);
-                }
-                console.log('-----NEXT CLICK--------')
-            }
+        while ((await getRunningScript()).length > 0) {
+            console.log('-------START WAIT CURRENT--------')
+            console.log(await getRunningScript())
+            let currentElement = await waitForCurrentElement();
+            console.log('-------END WAIT CURRENT--------')
+            await whitForElementIsShowingOnPage(currentElement);
+            console.log('-------- SHOW ON PAGE ----------')
+            clickToElement(currentElement)
+            console.log('-------CLICK--------')
+            await saveCurrentScriptRunning();
+            await whitForAnimation(timeAnimation);
+            console.log('-------ANIMATION--------')
         }
         console.log('-----END SCRIPTING-------')
-        isCanNextScriptAction = true;
-        sendMessageFromExtension(actionMame, {})
+        sendMessageFromBrowser(actionMame, {})
     });
     observer.observe(document.body, CONFIG_CHANGE_DOM);
 }
